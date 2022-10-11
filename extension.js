30831d2faa7c39ad8e02cc74e8a982c6cec8346d
takenX10 <frau.alessandro01@gmail.com>
2022-10-11 21:06:35 +0200

    working version, missing open on new tab and icons

--- extension.js
+++ extension.js
@@ -1,80 +1,76 @@
 const vscode = require('vscode');
+const pathLibrary = require('path');
+const fs = require('fs');
 const dirTree = require("directory-tree");
 
 const DIRECTORY_TYPE = "directory";
 const DIRECTORY_ICON = "$(folder)";
 const FILE_TYPE = "file";
-const FILE_ICON = "$(file)";
-const mypick = vscode.window.createQuickPick();
+const FILE_ICON = "$(git-commit)";
+let mypick = undefined;
 let currentBasePath = "";
 let currentFoldersList = [];
 let files = {};
 let currentFilesList = [];
 
-function getIcon(type){
-    return type == FILE_TYPE ? FILE_ICON: DIRECTORY_ICON;
+function getIcon(type) {
+    return type == FILE_TYPE ? FILE_ICON : DIRECTORY_ICON;
 }
 
-function transformToArray(current, path){
+function transformToArray(current, path) {
     let newArray = {};
-    for(let f of current){
+    for (let f of current) {
         newArray[f.name] = {
             label: `${getIcon(f.type)} ${f.name}`,
             description: `${path}${f.name}`,
             name: f.name,
             type: f.type
         }
-        if(f.type == DIRECTORY_TYPE){
+        if (f.type == DIRECTORY_TYPE) {
             newArray[f.name]["children"] = transformToArray(f.children, `${path}${f.name}/`);
+            newArray[f.name]["children"][".."] = {
+                label: `${DIRECTORY_ICON} ..`,
+                description: path.substring(0, path.length - 1),
+                type: DIRECTORY_TYPE,
+                name: ".."
+            };
+
         }
     }
     return newArray;
 }
 
-function sortFiles(currentFiles){
-    currentFiles = currentFiles.sort(function(a,b){
-        if(a.type == b.type){
+function sortFiles(currentFiles) {
+    currentFiles = currentFiles.sort(function (a, b) {
+        if (a.type == b.type) {
             return (a.label > b.label ? -1 : 1);
-        }else {
+        } else {
             return (a.type == DIRECTORY_TYPE ? -1 : 1);
         }
     });
-    let index = currentFiles.findIndex((v)=>{return v.name == ".."});
-    if(index != -1){
+    let index = currentFiles.findIndex((v) => { return v.name == ".." });
+    if (index != -1) {
         const removed = currentFiles.splice(index, 1);
         currentFiles = [removed[0], ...currentFiles];
     }
     return currentFiles;
 }
 
-function getFilesDictFromPath(path){
+function getFilesDictFromPath(path) {
     path = path.split("/");
     let currentPosition = files;
-    for(let p of path){
-        if(p != ''){
+    for (let p of path) {
+        if (p != '') {
             currentPosition = currentPosition[p].children;
         }
     }
     return currentPosition;
 }
 
-async function makeCurrentFolder(path){
+async function makeCurrentFolder(path) {
     currentFoldersList = [];
-    if(path != ""){
-        let current = path.split("/");
-        let final = "";
-        for(let i=0; i<current.length - 1; i++){
-            final += "/"+current[i];
-        }
-        currentFoldersList.push({
-            label: `${DIRECTORY_ICON} ..`,
-            description: final,
-            type: DIRECTORY_TYPE,
-            name: ".."
-        });
-    }
     const currentDict = getFilesDictFromPath(path);
-    Object.keys(currentDict).forEach((k)=>{
+    Object.keys(currentDict).forEach((k) => {
         currentFoldersList.push({
             label: currentDict[k].label,
             description: currentDict[k].description,
@@ -85,13 +81,13 @@ async function makeCurrentFolder(path){
     currentFoldersList = sortFiles(currentFoldersList);
 }
 
-function searchFile(path, val){
-    const regex = new RegExp(`${val}`,"gi");
+function searchFile(path, val) {
+    const regex = new RegExp(`${val}`, "gi");
     const currentDict = getFilesDictFromPath(path);
 
     let res = [];
-    Object.keys(currentDict).forEach((k)=>{
-        if (currentDict[k].name.match(regex)){
+    Object.keys(currentDict).forEach((k) => {
+        if (currentDict[k].name.match(regex)) {
             res.push({
                 label: currentDict[k].label,
                 description: currentDict[k].description,
@@ -99,8 +95,8 @@ function searchFile(path, val){
                 name: currentDict[k].name
             });
         }
-        if(currentDict[k].type == DIRECTORY_TYPE){
-            res = [...res, ...searchFile(`${path}${path != ""?"/":""}${currentDict[k].name}`, val)];
+        if (currentDict[k].type == DIRECTORY_TYPE && currentDict[k].name != "..") {
+            res = [...res, ...searchFile(`${path}${path != "" ? "/" : ""}${currentDict[k].name}`, val)];
         }
     });
     return res;
@@ -109,34 +105,43 @@ function searchFile(path, val){
 
 function activate(context) {
     const directory = (vscode.workspace.rootPath || "/");
-    const tempfiles = dirTree(directory, {attributes:["type"]});
+    const tempfiles = dirTree(directory, { attributes: ["type"] });
     files = transformToArray(tempfiles.children, "");
-    let disposable = vscode.commands.registerCommand( 'better-quick-open.quickopen', async function () {
+    function selectedValue(mySelection) {
+        const current = (mySelection || mypick.activeItems[0]);
+        if (current["type"] == DIRECTORY_TYPE) {
+            currentBasePath = current["description"];
+            makeCurrentFolder(currentBasePath);
+            mypick.value = "";
+            mypick.items = currentFoldersList;
+        } else {
+            const filePath = pathLibrary.join(vscode.workspace.rootPath, current.description);
+            const openPath = vscode.Uri.file(filePath);
+            mypick.dispose();
+            vscode.window.showTextDocument(openPath).then((editor) => { });
+        }
+    }
+    let disposable = vscode.commands.registerCommand('better-quick-open.quickopen', async function () {
+        mypick = vscode.window.createQuickPick();
+        mypick.ignoreFocusOut = true;
         currentBasePath = "";
         makeCurrentFolder(currentBasePath);
         mypick.items = currentFoldersList;
         mypick.show();
-        mypick.onDidChangeValue((val)=>{
-            if(val != ""){
-                currentFilesList = sortFiles(searchFile(currentBasePath, val));
-                mypick.items = currentFilesList;
-            }else{
+        mypick.onDidChangeValue((val) => {
+            if (val != "") {
+                if (val.substring(val.length - 1, val.length) == "/") {
+                    mypick.value = mypick.value.substring(0, mypick.value.length - 1);
+                    selectedValue(mypick.items.find((e) => { return e.name == mypick.value }));
+                }else{
+                    currentFilesList = sortFiles(searchFile(currentBasePath, val));
+                    mypick.items = currentFilesList;
+                }
+            } else {
                 mypick.items = currentFoldersList;
             }
         });
-        mypick.onDidAccept(()=>{
-            const current = mypick.activeItems[0];
-            console.log(current);
-            if(current["type"] == DIRECTORY_TYPE){
-                console.log("directory");
-                currentBasePath = current["description"];
-                makeCurrentFolder(currentBasePath);
-                mypick.value = "";
-                mypick.items = currentFoldersList;
-            }else{
-
-            }
-        })
+        mypick.onDidAccept(selectedValue);
     });
     context.subscriptions.push(disposable);
 }
